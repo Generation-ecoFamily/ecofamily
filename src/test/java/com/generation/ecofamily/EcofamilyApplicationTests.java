7c04859d0bb5118f3d36d8fcf9433fc365c70279
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-16 11:00:37 -0300

    Created class Categorias

---
+++ src/test/java/com/generation/ecofamily/EcofamilyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.ecofamily;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcofamilyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
