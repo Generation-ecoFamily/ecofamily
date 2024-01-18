290d058b4b10e70481319222a112f62efcd63823
carina-maleski <carinamaleski@gmail.com>
2024-01-18 11:13:28 -0300

    added delete method

--- src/main/java/com/generation/ecofamily/controller/CategoriaController.java
+++ src/main/java/com/generation/ecofamily/controller/CategoriaController.java
@@ -1,18 +1,21 @@
 package com.generation.ecofamily.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.ecofamily.model.Categoria;
 import com.generation.ecofamily.repository.CategoriaRepository;
@@ -56,5 +59,15 @@ public class CategoriaController {
 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
 	}*/
 	
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	@DeleteMapping("/{id}")
+	public void deleteById(@PathVariable Long id) {
+		Optional<Categoria> categoria = categoriaRepository.findById(id);
+		
+		if(categoria.isEmpty())
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+		
+		categoriaRepository.deleteById(id);
+	}
 	
 }
