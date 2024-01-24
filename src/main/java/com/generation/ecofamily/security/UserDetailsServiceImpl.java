d19b197234c9acc141ec2bf81b0669546fb953f9
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-24 12:21:05 -0300

    Created class UserDetailsServiceImpl

---
+++ src/main/java/com/generation/ecofamily/security/UserDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.generation.ecofamily.security;
+
+import com.generation.ecofamily.model.Usuario;
+import com.generation.ecofamily.repository.UsuarioRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UserDetailsServiceImpl {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    public UserDetails loadUserByUsername(String userName) throws UsernameNotFoundException {
+
+        Optional<Usuario> usuario = usuarioRepository.findByUsuario(userName);
+
+        if(usuario.isPresent())
+            return new UserDetailsImpl(usuario.get());
+        else
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN);
+    }
+}
