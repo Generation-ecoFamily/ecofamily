174c0e31962a2883f83008587b4f0186ba54517e
Giovanni Torelli <giovanni.walter@outlook.com>
2024-01-24 12:36:25 -0300

    Implemented UserDetailsService on class UserDetailsServiceImpl

--- src/main/java/com/generation/ecofamily/security/UserDetailsServiceImpl.java
+++ src/main/java/com/generation/ecofamily/security/UserDetailsServiceImpl.java
@@ -5,6 +5,7 @@ import com.generation.ecofamily.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -12,7 +13,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.Optional;
 
 @Service
-public class UserDetailsServiceImpl {
+public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
