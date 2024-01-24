7b19b2e5985fcd400b9bd62eb956f2275edf9637
livehass <silva.felipe12@hotmail.com>
2024-01-24 15:05:51 -0300

    Created new UsuarioService and they methods

---
+++ src/main/java/com/generation/ecofamily/service/UsuarioService.java
@@ -0,0 +1,90 @@
+package com.generation.ecofamily.service;
+
+import com.generation.ecofamily.model.Usuario;
+import com.generation.ecofamily.model.UsuarioLogin;
+import com.generation.ecofamily.repository.UsuarioRepository;
+import com.generation.ecofamily.security.JwtService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Optional;
+
+@Service
+public class UsuarioService {
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public Optional<Usuario> cadastrarUsuario(Usuario usuario) {
+
+        if (usuarioRepository.findByEmail(usuario.getEmail()).isPresent())
+            return Optional.empty();
+
+        usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+        return Optional.of(usuarioRepository.save(usuario));
+    }
+
+    public Optional<Usuario> atualizarUsuario(Usuario usuario) {
+        if (usuarioRepository.findById(usuario.getId()).isPresent()) {
+            Optional<Usuario> buscaEmail = usuarioRepository.findByEmail(usuario.getEmail());
+
+            if ((buscaEmail.isPresent()) && (buscaEmail.get().getId() != usuario.getId()))
+                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Email já cadastrado", null);
+
+            usuario.setSenha(criptografarSenha(usuario.getSenha()));
+
+            return Optional.ofNullable(usuarioRepository.save(usuario));
+        }
+        return Optional.empty();
+    }
+
+    public Optional<UsuarioLogin> autenticarUsuario(Optional<UsuarioLogin> usuarioLogin) {
+
+        var credenciais = new UsernamePasswordAuthenticationToken(usuarioLogin.get().getEmail(), usuarioLogin.get().getSenha());
+
+        Authentication authentication = authenticationManager.authenticate(credenciais);
+
+        if (authentication.isAuthenticated()) {
+            Optional<Usuario> usuario = usuarioRepository.findByEmail(usuarioLogin.get().getEmail());
+
+            if (usuario.isPresent()) {
+
+                usuarioLogin.get().setId(usuario.get().getId());
+                usuarioLogin.get().setNome(usuario.get().getNome());
+                usuarioLogin.get().setFoto(usuario.get().getFoto());
+                usuarioLogin.get().setToken(gerarToken(usuarioLogin.get().getEmail()));
+                usuarioLogin.get().setSenha("");
+
+                return usuarioLogin;
+            }
+        }
+        return Optional.empty();
+    }
+
+    private String criptografarSenha(String senha) {
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        return encoder.encode(senha);
+    }
+
+    private String gerarToken(String usuario) {
+        return "Bearer " + jwtService.generateToken(usuario);
+    }
+
+
+}
